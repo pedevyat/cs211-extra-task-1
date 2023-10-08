87a1ad04efbe5870b7286158d6e8851d0998d038
Kostya <114941602+pedevyat@users.noreply.github.com>
2023-10-08 13:03:47 +0300

    Complete first 3 functions

--- Bonus/extra-task-1.h
+++ Bonus/extra-task-1.h
@@ -1,1 +1,11 @@
 #pragma once
+
+// Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
+double seconds_difference(double, double);
+
+// Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
+double hours_difference(double, double);
+
+// Return the total number of hours in the specified number of hours, minutes, and seconds.
+double to_float_hours(int, int, int);
+
--- Bonus/main.cpp
+++ Bonus/main.cpp
@@ -1,3 +1,24 @@
 #include "extra-task-1.h"
+#include <assert.h>
+#include <iostream>
 
 using namespace std;
+
+int main()
+{
+	// seconds_difference
+	assert(seconds_difference(1800.0, 3600.0) == 1800);
+	assert(seconds_difference(3600.0, 1800.0) == -1800);
+	assert(seconds_difference(1800.0, 2160.0) == 360);
+	assert(seconds_difference(1800.0, 1800.0) == 0);
+	// hours_difference
+	assert(hours_difference(1800.0, 3600.0) == 0.5);
+	assert(hours_difference(3600.0, 1800.0) == -0.5);
+	assert(hours_difference(1800.0, 2160.0) == 0.1);
+	assert(hours_difference(1800.0, 1800.0) == 0);
+	// to_float_hours
+	assert(to_float_hours(0, 15, 0) == 0.25);
+	assert(to_float_hours(2, 45, 9) == 2.7525);
+	assert(to_float_hours(1, 0, 36) == 1.01);
+
+}
