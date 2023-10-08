bc0ee6f7bf231fb0e9ae75804050e160ac98486e
Kostya <114941602+pedevyat@users.noreply.github.com>
2023-10-08 18:26:03 +0300

    complete to_24_hour_clock

--- Bonus/extra-task-1.h
+++ Bonus/extra-task-1.h
@@ -9,3 +9,5 @@ double hours_difference(double, double);
 // Return the total number of hours in the specified number of hours, minutes, and seconds.
 double to_float_hours(int, int, int);
 
+// hours is a number of hours since midnight. Return the hour as seen on a 24 - hour clock.
+double to_24_hour_clock(double);
